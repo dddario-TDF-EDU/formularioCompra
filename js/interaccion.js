8e0d405d756329484a70815fdf078431d84b70fa
= <dariocabrera@tdf.edu.ar>
2022-08-22 10:07:41 -0300

    cambios del dia lunes

--- js/interaccion.js
+++ js/interaccion.js
@@ -1,23 +1,24 @@
-//quiero tomar el valor del rango y asignarlo a una variable
-
-let eleccionKG = document.querySelector("#cantKG div input").value;
-
 //quiero tomar el valor en "vivo"
 
-let valorVivo;
+let valorVivo = Number(document.querySelector("#cantKG div input").value);;
 let rango = document.querySelector("#cantKG div input");
 
-//codigo robado
 
-let textoPrueba = document.createTextNode("");
+//Funcion del rango de KG
 
-rango.addEventListener("change", function() {
-    valorVivo =  document.querySelector("#cantKG div input").value;
-    textoPrueba = document.querySelector("#cantKG p").innerHTML = "Usted eligio " + valorVivo  +"kg, puede seleccionar hasta "+ maxSabores(valorVivo) +" sabores.";
+        //codigo robado que cheque y actualiza el valor en TIEMPO "real"
 
-},false);
+        let textoPrueba = document.createTextNode("");
 
-// asignar funcion click botones y desaparecer
+        rango.addEventListener("change", function() {
+            valorVivo =  Number(document.querySelector("#cantKG div input").value);
+            textoPrueba = document.querySelector("#cantKG p").innerText = "Usted eligio " + valorVivo  +"kg, puede seleccionar hasta "+ maxSabores(valorVivo) +" sabores.";
+
+        },false);
+
+///        
+
+// Funciones del boton "Armar mi pedido"
 
 let btnIntro=document.querySelector("div .esteticaBoton");
 
@@ -27,27 +28,34 @@ btnIntro.addEventListener('click' ,() => {
     btnIntro.classList.toggle("ocultarSINO");
 })
 
-//funciones del  boton confirmar ciudad
+//Funciones del  boton confirmar ciudad
 
-let btnConfirmar=document.querySelector("#ciudadEntrega button");
+let btnConfirmar =document.querySelector("#ciudadEntrega button");
 let ciudadElegida;
+let btnSabores = document.querySelector("#collapseOne div button");
 
 btnConfirmar.addEventListener('click' ,() => {
+    let hayCiudad = false;
     ciudadEnvio = document.querySelectorAll("#ciudadEntrega div div input");
      for (let i=0;i<ciudadEnvio.length;i++){
          if (ciudadEnvio[i].checked==true){
              ciudadElegida=ciudadEnvio[i].value;
+             hayCiudad = true;
              break;
          } 
      }
-    if (ciudadElegida==0){
+    if (hayCiudad===false){
         alert("usted no selecciono una ciudad");
     } else {
         document.querySelector("#ciudadEntrega").classList.toggle("ocultarSINO");
         document.querySelector("#armadoPedido").classList.toggle("ocultarSINO");
+
+        //solucion parcial a la aparicion por defecto de la seccion HELADO POR KG 
+        
+        document.querySelector("#collapseOne").classList.toggle("show");
+
         document.querySelector("#armadoPedido h2 span").innerText=(ciudadElegida);
         let KGselect = document.createElement("p");
-        //NOSE COMO ARREGLAR ESTO SIN EMPEZAR DESDE UN VALOR DEFAULT
         textoPrueba = document.createTextNode("Usted eligio 1 kg, puede seleccionar hasta 4 sabores");
         KGselect.appendChild(textoPrueba);
         document.querySelector("#cantKG").appendChild(KGselect);
@@ -63,7 +71,7 @@ const cantMaxSabores = [3,4,6,8];
 
 function maxSabores(KG) {
 
-        switch (Number(KG)) {
+        switch (KG) {
             case 0.5:
                 return cantMaxSabores[0];
             case 1:
@@ -78,4 +86,80 @@ function maxSabores(KG) {
         }
 }
 
-//
+//FUNCION btnSabores
+
+btnSabores.addEventListener('click' ,()=> {
+    document.querySelector("#collapseOne div .ocultarSINO").classList.toggle("ocultarSINO");
+
+    // COSAS QUE PUEDEN VOLVER A APARECER
+    document.querySelector("#collapseOne div label").classList.toggle("ocultarSINO");
+    document.querySelector("#cantKG").classList.toggle("ocultarSINO");
+    document.querySelector("#collapseOne div div button").classList.toggle("ocultarSINO");
+})
+
+//Seccion para controlar la eleccion de sabores
+
+let saboresDisponibles = ['Chocolate','Cho. con almendras','Vainilla','Coco','Fru. del bosque','Banana Split','Granizado','Tiramisú','Cereza','DDL Granizado'];
+
+let variableElecciones = new Array (10); //[0,1,2,3,4,5,6,7,8,9];
+
+let eleccionCliente = document.querySelectorAll("#armadoPedido .saborElegido");
+
+let formSabores = document.querySelector("#armadoPedido .seccionSabor");
+
+let btnConfirmarSabores = document.querySelector("#armadoPedido .seccionSabor button");
+
+for (let i=0;i<variableElecciones.length;i++){
+        variableElecciones[i]=false;
+    }
+
+// formSabores.addEventListener("change", function() {
+//     for (let i=0;i<variableElecciones.length;i++){
+//         if(eleccionCliente[i].checked=="true"){
+//            variableElecciones[i]=true;
+//         } else {
+//            variableElecciones[i]=false;
+//         }
+//     }
+// });
+
+btnConfirmarSabores.addEventListener("click" , ()=>{
+    let totalElegidos = 0;
+    let condicionCompra = false
+    for (let i=0;i<variableElecciones.length;i++){
+        variableElecciones[i]=eleccionCliente[i].checked;
+        if (variableElecciones[i]==true){
+            totalElegidos++;
+            console.log(totalElegidos)
+        }
+    }
+    
+    if (totalElegidos<1){
+        alert("debe elegir al menos un sabor")
+    } else if (totalElegidos>valorVivo){
+        alert("usted no puede elegir más de "+ maxSabores(valorVivo) +" sabores")
+    } else {
+        condicionCompra=true;
+    }
+
+    if (condicionCompra===true){
+        
+    }
+
+
+})
+
+
+
+
+//sabores seleccionables OK
+//sabores elegidos por el cliente OK
+//valor de elegido de la variable 
+ 
+//relacionar al click las dos anteriores
+    //puedo detectar cuando se hace click
+
+//array total de esa eleccion
+
+
+
